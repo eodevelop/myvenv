9788745fc7f7b322cce87bb3ab83c7ca7a60258c
eodevelop <eodevelop@naver.com>
2020-03-23 15:07:23 +0900

    add blog directory

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'g@ucg686gb#pq8@(^#h$2z%ni)8jy9y@smm&r#bf64d3$#am!5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
